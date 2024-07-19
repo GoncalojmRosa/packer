c0eefe1fc218aaadd4e450e5b357f0fe42a77215
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-07-19 20:47:39 +0100

    Initial style of main page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export function Navbar() {
+  return (
+    <div className="flex items-center justify-between h-24 text-lg">
+      <div className="flex items-center">
+        <span className="text-6xl">📦</span>
+      </div>
+      <button>Login</button>
+    </div>
+  );
+}
